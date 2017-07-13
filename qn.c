f63bee5516ddd36175107eb902397b6b4591dad0
Uday Singh <udaysingh.code@gmail.com>
2017-07-13 16:32:39 -0400

    added break condition, parity checking, and strip 8th bit of each input byte

--- qn.c
+++ qn.c
@@ -16,8 +16,9 @@ void startRawMode() {
   // read attributes into struct
   struct termios raw = orig_termios;
   // modify local flags by flipping fourth bit to not print typing / quit at q
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP |  IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   // apply to terminal
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
